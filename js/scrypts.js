ba83288e6569388281b82fb1bf87f4a07b3b03bc
Radoslav-Atanasov <78895518+Radoslav-Atanasov@users.noreply.github.com>
2021-04-25 18:28:58 +0300

    commit

--- js/scrypts.js
+++ js/scrypts.js
@@ -1,1 +1,46 @@
-console.log("zashto?")
+
+
+const xhr = new XMLHttpRequest();
+const container = document.getElementById("ajaxContainer");
+xhr.onload = function(){
+	if (this.status === 200) {
+		container.innerHTML = xhr.responseText;
+	} else {
+		console.warn("Did not recieve 200 OK from response!");
+	}
+};
+xhr.open("get", "../ajaxxx.html");
+xhr.send();
+
+
+
+// Fade in and out in plans
+$(document).ready(function(){
+	$(".option-container-B").hide();
+	$(".btn-services1").addClass("active-btn");
+});
+$(".btn-services1").on("click", function(){
+	$(".option-container-B").fadeOut();
+	$(".option-container-A").fadeIn();
+	$(".btn-services1").addClass("active-btn");
+	$(".btn-services2").removeClass("active-btn");
+});
+$(".btn-services2").on("click", function(){
+	$(".option-container-A").fadeOut();
+	$(".option-container-B").fadeIn();
+	$(".btn-services1").removeClass("active-btn");
+	$(".btn-services2").addClass("active-btn");
+});
+
+//toggle dropdown menu in Blog 
+$(document).ready(function(){
+	$("#dropdownMenu").hide();
+})
+$(".three-dots").on("click", function(){
+	if ($("#dropdownMenu").is("hide()")) {
+		$("#dropdownMenu").show();
+		console.log("baaasi");
+	} else {
+		$("#dropdownMenu").hide();
+	}
+});
